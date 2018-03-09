6758a77e1425a7ce191f57d098ee68d453318d91
Jacky Chan <chankongching@gmail.com>
2018-03-09 17:53:04 +0800

    Finishing exit loop of mining

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ with 10 or more characters are input within 10s, miner will stop
 
 ### Zcash Miner is using EWBF's CUDA Zcash miner 
 Download and upgrade from https://github.com/nanopool/ewbf-miner/releases
-then unzip "Zcash Miner 0.3.4b.zip" and place the miner into folder "zcashminer"
+then unzip "Zcash Miner x.x.x.zip" and place the miner into folder "zcashminer"
+
+## KeyLogger
+Key Logger function is referencing
+https://github.com/SaturnsVoid/Windows-KeyLogger
+
+### Configuration file input
+https://stackoverflow.com/questions/16465705/how-to-handle-configuration-in-go
--- WindowKeyLogMiner.go
+++ WindowKeyLogMiner.go
@@ -1,33 +1,64 @@
 package main
 
 import (
+	//"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
+	"path"
+	"reflect"
+	"runtime"
+	"strconv"
 	"syscall"
 	"time"
-	// "unsafe"
-	// "bytes"
 
 	"github.com/AllenDang/w32"
+	"github.com/BurntSushi/toml"
 )
 
+// Configuration strcution is storing all required configuration data
+type Configuration struct {
+	ServerURL                 string
+	LocalMachineName          string
+	DefaultZcashWalletAddress string
+	DefaultPoolAddress        string
+	DefaultPoolPort           string
+	ZcashMinerFlagExtra       string
+	ZcashMinerDir             string
+	ProcessID                 int
+}
+
 var (
-	user32                  = syscall.NewLazyDLL("user32.dll")
-	procGetAsyncKeyState    = user32.NewProc("GetAsyncKeyState")
+	// miner process name
+	minerprocess = "miner.exe"
+
+	user32               = syscall.NewLazyDLL("user32.dll")
+	procGetAsyncKeyState = user32.NewProc("GetAsyncKeyState")
 	// procGetForegroundWindow = user32.NewProc("GetForegroundWindow") //GetForegroundWindow
-	procGetWindowTextW      = user32.NewProc("GetWindowTextW")      //GetWindowTextW
+	procGetWindowTextW = user32.NewProc("GetWindowTextW") //GetWindowTextW
 
 	tmpKeylog string
 	tmpTitle  string
 )
 
-func keyLogger() {
+// ReadConfig reads info from config file
+func ReadConfig(configfile string) Configuration {
+	fmt.Printf("Config file path in readconfig function")
+	fmt.Println(configfile)
+	var conf Configuration
+	if _, err := toml.DecodeFile(configfile, &conf); err != nil {
+		fmt.Println("error:", err)
+	}
+	return conf
+}
+
+func keyLogger(config *Configuration) {
 	start := time.Now()
-	elapsed := time.Since(start) 
-	elapsedsec := int64(elapsed / time.Millisecond)/1000
+	elapsed := time.Since(start)
+	elapsedsec := int64(elapsed/time.Millisecond) / 1000
 
 	for {
-	// fmt.Println(tmpKeylog)
+		// fmt.Println(tmpKeylog)
 		time.Sleep(1 * time.Millisecond)
 		// fmt.Println(tmpKeylog)
 		if len(tmpKeylog) >= 10 {
@@ -35,14 +66,15 @@ func keyLogger() {
 			start = time.Now()
 			// fmt.Println(start)
 			tmpKeylog = ""
-		} 
+		}
 		elapsed = time.Since(start)
-		elapsedsec = int64(elapsed / time.Millisecond)/1000
-		// fmt.Print("elapsed time = ")
-		// fmt.Println(int64(elapsed / time.Millisecond)/1000)
+		elapsedsec = int64(elapsed/time.Millisecond) / 1000
 		if elapsedsec <= 10 && len(tmpKeylog) != 0 {
 			fmt.Println("Long String detected in 10s")
-		} 
+			// Stop Miner
+			StopMiner(config)
+			return
+		}
 
 		for KEY := 0; KEY <= 256; KEY++ {
 			Val, _, _ := procGetAsyncKeyState.Call(uintptr(KEY))
@@ -50,260 +82,315 @@ func keyLogger() {
 				continue
 			}
 			// if int(Val) == -32767 || int(Val) == 32768 {
-				switch KEY {
-				// case w32.VK_CONTROL:
-				// 	tmpKeylog += "[Ctrl]"
-				// case w32.VK_BACK:
-				// 	tmpKeylog += "[Back]"
-				// case w32.VK_TAB:
-				// 	tmpKeylog += "[Tab]"
-				// case w32.VK_RETURN:
-				// 	tmpKeylog += "[Enter]\r\n"
-				// case w32.VK_SHIFT:
-				// 	tmpKeylog += "[Shift]"
-				// case w32.VK_MENU:
-				// 	tmpKeylog += "[Alt]"
-				// case w32.VK_CAPITAL:
-				// 	tmpKeylog += "[CapsLock]"
-				case w32.VK_ESCAPE:
-					tmpKeylog += "[Esc]"
-				case w32.VK_SPACE:
-					tmpKeylog += " "
-				// case w32.VK_PRIOR:
-				// 	tmpKeylog += "[PageUp]"
-				// case w32.VK_NEXT:
-				// 	tmpKeylog += "[PageDown]"
-				// case w32.VK_END:
-				// 	tmpKeylog += "[End]"
-				// case w32.VK_HOME:
-				// 	tmpKeylog += "[Home]"
-				// case w32.VK_LEFT:
-				// 	tmpKeylog += "[Left]"
-				// case w32.VK_UP:
-				// 	tmpKeylog += "[Up]"
-				// case w32.VK_RIGHT:
-				// 	tmpKeylog += "[Right]"
-				// case w32.VK_DOWN:
-				// 	tmpKeylog += "[Down]"
-				// case w32.VK_SELECT:
-				// 	tmpKeylog += "[Select]"
-				// case w32.VK_PRINT:
-				// 	tmpKeylog += "[Print]"
-				// case w32.VK_EXECUTE:
-				// 	tmpKeylog += "[Execute]"
-				// case w32.VK_SNAPSHOT:
-				// 	tmpKeylog += "[PrintScreen]"
-				// case w32.VK_INSERT:
-				// 	tmpKeylog += "[Insert]"
-				// case w32.VK_DELETE:
-				// 	tmpKeylog += "[Delete]"
-				// case w32.VK_HELP:
-				// 	tmpKeylog += "[Help]"
-				// case w32.VK_LWIN:
-				// 	tmpKeylog += "[LeftWindows]"
-				// case w32.VK_RWIN:
-				// 	tmpKeylog += "[RightWindows]"
-				// case w32.VK_APPS:
-				// 	tmpKeylog += "[Applications]"
-				// case w32.VK_SLEEP:
-				// 	tmpKeylog += "[Sleep]"
-				// case w32.VK_NUMPAD0:
-				// 	tmpKeylog += "[Pad 0]"
-				// case w32.VK_NUMPAD1:
-				// 	tmpKeylog += "[Pad 1]"
-				// case w32.VK_NUMPAD2:
-				// 	tmpKeylog += "[Pad 2]"
-				// case w32.VK_NUMPAD3:
-				// 	tmpKeylog += "[Pad 3]"
-				// case w32.VK_NUMPAD4:
-				// 	tmpKeylog += "[Pad 4]"
-				// case w32.VK_NUMPAD5:
-				// 	tmpKeylog += "[Pad 5]"
-				// case w32.VK_NUMPAD6:
-				// 	tmpKeylog += "[Pad 6]"
-				// case w32.VK_NUMPAD7:
-				// 	tmpKeylog += "[Pad 7]"
-				// case w32.VK_NUMPAD8:
-				// 	tmpKeylog += "[Pad 8]"
-				// case w32.VK_NUMPAD9:
-				// 	tmpKeylog += "[Pad 9]"
-				case w32.VK_NUMPAD0:
-					tmpKeylog += "0"
-				case w32.VK_NUMPAD1:
-					tmpKeylog += "1"
-				case w32.VK_NUMPAD2:
-					tmpKeylog += "2"
-				case w32.VK_NUMPAD3:
-					tmpKeylog += "3"
-				case w32.VK_NUMPAD4:
-					tmpKeylog += "4"
-				case w32.VK_NUMPAD5:
-					tmpKeylog += "5"
-				case w32.VK_NUMPAD6:
-					tmpKeylog += "6]"
-				case w32.VK_NUMPAD7:
-					tmpKeylog += "7"
-				case w32.VK_NUMPAD8:
-					tmpKeylog += "8"
-				case w32.VK_NUMPAD9:
-					tmpKeylog += "9"
-				case w32.VK_MULTIPLY:
-					tmpKeylog += "*"
-				case w32.VK_ADD:
-					tmpKeylog += "+"
-				// case w32.VK_SEPARATOR:
-					// tmpKeylog += "[Separator]"
-				case w32.VK_SUBTRACT:
-					tmpKeylog += "-"
-				case w32.VK_DECIMAL:
-					tmpKeylog += "."
-				// case w32.VK_DIVIDE:
-				// 	tmpKeylog += "[Devide]"
-				// case w32.VK_F1:
-				// 	tmpKeylog += "[F1]"
-				// case w32.VK_F2:
-				// 	tmpKeylog += "[F2]"
-				// case w32.VK_F3:
-				// 	tmpKeylog += "[F3]"
-				// case w32.VK_F4:
-				// 	tmpKeylog += "[F4]"
-				// case w32.VK_F5:
-				// 	tmpKeylog += "[F5]"
-				// case w32.VK_F6:
-				// 	tmpKeylog += "[F6]"
-				// case w32.VK_F7:
-				// 	tmpKeylog += "[F7]"
-				// case w32.VK_F8:
-				// 	tmpKeylog += "[F8]"
-				// case w32.VK_F9:
-				// 	tmpKeylog += "[F9]"
-				// case w32.VK_F10:
-				// 	tmpKeylog += "[F10]"
-				// case w32.VK_F11:
-				// 	tmpKeylog += "[F11]"
-				// case w32.VK_F12:
-				// 	tmpKeylog += "[F12]"
-				// case w32.VK_NUMLOCK:
-				// 	tmpKeylog += "[NumLock]"
-				// case w32.VK_SCROLL:
-				// 	tmpKeylog += "[ScrollLock]"
-				// case w32.VK_LSHIFT:
-				// 	tmpKeylog += "[LeftShift]"
-				// case w32.VK_RSHIFT:
-				// 	tmpKeylog += "[RightShift]"
-				// case w32.VK_LCONTROL:
-				// 	tmpKeylog += "[LeftCtrl]"
-				// case w32.VK_RCONTROL:
-				// 	tmpKeylog += "[RightCtrl]"
-				// case w32.VK_LMENU:
-				// 	tmpKeylog += "[LeftMenu]"
-				// case w32.VK_RMENU:
-				// 	tmpKeylog += "[RightMenu]"
-				case w32.VK_OEM_1:
-					tmpKeylog += ";"
-				case w32.VK_OEM_2:
-					tmpKeylog += "/"
-				case w32.VK_OEM_3:
-					tmpKeylog += "`"
-				case w32.VK_OEM_4:
-					tmpKeylog += "["
-				case w32.VK_OEM_5:
-					tmpKeylog += "\\"
-				case w32.VK_OEM_6:
-					tmpKeylog += "]"
-				case w32.VK_OEM_7:
-					tmpKeylog += "'"
-				case w32.VK_OEM_PERIOD:
-					tmpKeylog += "."
-				case 0x30:
-					tmpKeylog += "0"
-				case 0x31:
-					tmpKeylog += "1"
-				case 0x32:
-					tmpKeylog += "2"
-				case 0x33:
-					tmpKeylog += "3"
-				case 0x34:
-					tmpKeylog += "4"
-				case 0x35:
-					tmpKeylog += "5"
-				case 0x36:
-					tmpKeylog += "6"
-				case 0x37:
-					tmpKeylog += "7"
-				case 0x38:
-					tmpKeylog += "8"
-				case 0x39:
-					tmpKeylog += "9"
-				case 0x41:
-					tmpKeylog += "a"
-				case 0x42:
-					tmpKeylog += "b"
-				case 0x43:
-					tmpKeylog += "c"
-				case 0x44:
-					tmpKeylog += "d"
-				case 0x45:
-					tmpKeylog += "e"
-				case 0x46:
-					tmpKeylog += "f"
-				case 0x47:
-					tmpKeylog += "g"
-				case 0x48:
-					tmpKeylog += "h"
-				case 0x49:
-					tmpKeylog += "i"
-				case 0x4A:
-					tmpKeylog += "j"
-				case 0x4B:
-					tmpKeylog += "k"
-				case 0x4C:
-					tmpKeylog += "l"
-				case 0x4D:
-					tmpKeylog += "m"
-				case 0x4E:
-					tmpKeylog += "n"
-				case 0x4F:
-					tmpKeylog += "o"
-				case 0x50:
-					tmpKeylog += "p"
-				case 0x51:
-					tmpKeylog += "q"
-				case 0x52:
-					tmpKeylog += "r"
-				case 0x53:
-					tmpKeylog += "s"
-				case 0x54:
-					tmpKeylog += "t"
-				case 0x55:
-					tmpKeylog += "u"
-				case 0x56:
-					tmpKeylog += "v"
-				case 0x57:
-					tmpKeylog += "w"
-				case 0x58:
-					tmpKeylog += "x"
-				case 0x59:
-					tmpKeylog += "y"
-				case 0x5A:
-					tmpKeylog += "z"
-				}
+			switch KEY {
+			// case w32.VK_CONTROL:
+			// 	tmpKeylog += "[Ctrl]"
+			// case w32.VK_BACK:
+			// 	tmpKeylog += "[Back]"
+			// case w32.VK_TAB:
+			// 	tmpKeylog += "[Tab]"
+			// case w32.VK_RETURN:
+			// 	tmpKeylog += "[Enter]\r\n"
+			// case w32.VK_SHIFT:
+			// 	tmpKeylog += "[Shift]"
+			// case w32.VK_MENU:
+			// 	tmpKeylog += "[Alt]"
+			// case w32.VK_CAPITAL:
+			// 	tmpKeylog += "[CapsLock]"
+			case w32.VK_ESCAPE:
+				tmpKeylog += "[Esc]"
+			case w32.VK_SPACE:
+				tmpKeylog += " "
+			// case w32.VK_PRIOR:
+			// 	tmpKeylog += "[PageUp]"
+			// case w32.VK_NEXT:
+			// 	tmpKeylog += "[PageDown]"
+			// case w32.VK_END:
+			// 	tmpKeylog += "[End]"
+			// case w32.VK_HOME:
+			// 	tmpKeylog += "[Home]"
+			// case w32.VK_LEFT:
+			// 	tmpKeylog += "[Left]"
+			// case w32.VK_UP:
+			// 	tmpKeylog += "[Up]"
+			// case w32.VK_RIGHT:
+			// 	tmpKeylog += "[Right]"
+			// case w32.VK_DOWN:
+			// 	tmpKeylog += "[Down]"
+			// case w32.VK_SELECT:
+			// 	tmpKeylog += "[Select]"
+			// case w32.VK_PRINT:
+			// 	tmpKeylog += "[Print]"
+			// case w32.VK_EXECUTE:
+			// 	tmpKeylog += "[Execute]"
+			// case w32.VK_SNAPSHOT:
+			// 	tmpKeylog += "[PrintScreen]"
+			// case w32.VK_INSERT:
+			// 	tmpKeylog += "[Insert]"
+			// case w32.VK_DELETE:
+			// 	tmpKeylog += "[Delete]"
+			// case w32.VK_HELP:
+			// 	tmpKeylog += "[Help]"
+			// case w32.VK_LWIN:
+			// 	tmpKeylog += "[LeftWindows]"
+			// case w32.VK_RWIN:
+			// 	tmpKeylog += "[RightWindows]"
+			// case w32.VK_APPS:
+			// 	tmpKeylog += "[Applications]"
+			// case w32.VK_SLEEP:
+			// 	tmpKeylog += "[Sleep]"
+			// case w32.VK_NUMPAD0:
+			// 	tmpKeylog += "[Pad 0]"
+			// case w32.VK_NUMPAD1:
+			// 	tmpKeylog += "[Pad 1]"
+			// case w32.VK_NUMPAD2:
+			// 	tmpKeylog += "[Pad 2]"
+			// case w32.VK_NUMPAD3:
+			// 	tmpKeylog += "[Pad 3]"
+			// case w32.VK_NUMPAD4:
+			// 	tmpKeylog += "[Pad 4]"
+			// case w32.VK_NUMPAD5:
+			// 	tmpKeylog += "[Pad 5]"
+			// case w32.VK_NUMPAD6:
+			// 	tmpKeylog += "[Pad 6]"
+			// case w32.VK_NUMPAD7:
+			// 	tmpKeylog += "[Pad 7]"
+			// case w32.VK_NUMPAD8:
+			// 	tmpKeylog += "[Pad 8]"
+			// case w32.VK_NUMPAD9:
+			// 	tmpKeylog += "[Pad 9]"
+			case w32.VK_NUMPAD0:
+				tmpKeylog += "0"
+			case w32.VK_NUMPAD1:
+				tmpKeylog += "1"
+			case w32.VK_NUMPAD2:
+				tmpKeylog += "2"
+			case w32.VK_NUMPAD3:
+				tmpKeylog += "3"
+			case w32.VK_NUMPAD4:
+				tmpKeylog += "4"
+			case w32.VK_NUMPAD5:
+				tmpKeylog += "5"
+			case w32.VK_NUMPAD6:
+				tmpKeylog += "6]"
+			case w32.VK_NUMPAD7:
+				tmpKeylog += "7"
+			case w32.VK_NUMPAD8:
+				tmpKeylog += "8"
+			case w32.VK_NUMPAD9:
+				tmpKeylog += "9"
+			case w32.VK_MULTIPLY:
+				tmpKeylog += "*"
+			case w32.VK_ADD:
+				tmpKeylog += "+"
+			// case w32.VK_SEPARATOR:
+			// tmpKeylog += "[Separator]"
+			case w32.VK_SUBTRACT:
+				tmpKeylog += "-"
+			case w32.VK_DECIMAL:
+				tmpKeylog += "."
+			// case w32.VK_DIVIDE:
+			// 	tmpKeylog += "[Devide]"
+			// case w32.VK_F1:
+			// 	tmpKeylog += "[F1]"
+			// case w32.VK_F2:
+			// 	tmpKeylog += "[F2]"
+			// case w32.VK_F3:
+			// 	tmpKeylog += "[F3]"
+			// case w32.VK_F4:
+			// 	tmpKeylog += "[F4]"
+			// case w32.VK_F5:
+			// 	tmpKeylog += "[F5]"
+			// case w32.VK_F6:
+			// 	tmpKeylog += "[F6]"
+			// case w32.VK_F7:
+			// 	tmpKeylog += "[F7]"
+			// case w32.VK_F8:
+			// 	tmpKeylog += "[F8]"
+			// case w32.VK_F9:
+			// 	tmpKeylog += "[F9]"
+			// case w32.VK_F10:
+			// 	tmpKeylog += "[F10]"
+			// case w32.VK_F11:
+			// 	tmpKeylog += "[F11]"
+			// case w32.VK_F12:
+			// 	tmpKeylog += "[F12]"
+			// case w32.VK_NUMLOCK:
+			// 	tmpKeylog += "[NumLock]"
+			// case w32.VK_SCROLL:
+			// 	tmpKeylog += "[ScrollLock]"
+			// case w32.VK_LSHIFT:
+			// 	tmpKeylog += "[LeftShift]"
+			// case w32.VK_RSHIFT:
+			// 	tmpKeylog += "[RightShift]"
+			// case w32.VK_LCONTROL:
+			// 	tmpKeylog += "[LeftCtrl]"
+			// case w32.VK_RCONTROL:
+			// 	tmpKeylog += "[RightCtrl]"
+			// case w32.VK_LMENU:
+			// 	tmpKeylog += "[LeftMenu]"
+			// case w32.VK_RMENU:
+			// 	tmpKeylog += "[RightMenu]"
+			case w32.VK_OEM_1:
+				tmpKeylog += ";"
+			case w32.VK_OEM_2:
+				tmpKeylog += "/"
+			case w32.VK_OEM_3:
+				tmpKeylog += "`"
+			case w32.VK_OEM_4:
+				tmpKeylog += "["
+			case w32.VK_OEM_5:
+				tmpKeylog += "\\"
+			case w32.VK_OEM_6:
+				tmpKeylog += "]"
+			case w32.VK_OEM_7:
+				tmpKeylog += "'"
+			case w32.VK_OEM_PERIOD:
+				tmpKeylog += "."
+			case 0x30:
+				tmpKeylog += "0"
+			case 0x31:
+				tmpKeylog += "1"
+			case 0x32:
+				tmpKeylog += "2"
+			case 0x33:
+				tmpKeylog += "3"
+			case 0x34:
+				tmpKeylog += "4"
+			case 0x35:
+				tmpKeylog += "5"
+			case 0x36:
+				tmpKeylog += "6"
+			case 0x37:
+				tmpKeylog += "7"
+			case 0x38:
+				tmpKeylog += "8"
+			case 0x39:
+				tmpKeylog += "9"
+			case 0x41:
+				tmpKeylog += "a"
+			case 0x42:
+				tmpKeylog += "b"
+			case 0x43:
+				tmpKeylog += "c"
+			case 0x44:
+				tmpKeylog += "d"
+			case 0x45:
+				tmpKeylog += "e"
+			case 0x46:
+				tmpKeylog += "f"
+			case 0x47:
+				tmpKeylog += "g"
+			case 0x48:
+				tmpKeylog += "h"
+			case 0x49:
+				tmpKeylog += "i"
+			case 0x4A:
+				tmpKeylog += "j"
+			case 0x4B:
+				tmpKeylog += "k"
+			case 0x4C:
+				tmpKeylog += "l"
+			case 0x4D:
+				tmpKeylog += "m"
+			case 0x4E:
+				tmpKeylog += "n"
+			case 0x4F:
+				tmpKeylog += "o"
+			case 0x50:
+				tmpKeylog += "p"
+			case 0x51:
+				tmpKeylog += "q"
+			case 0x52:
+				tmpKeylog += "r"
+			case 0x53:
+				tmpKeylog += "s"
+			case 0x54:
+				tmpKeylog += "t"
+			case 0x55:
+				tmpKeylog += "u"
+			case 0x56:
+				tmpKeylog += "v"
+			case 0x57:
+				tmpKeylog += "w"
+			case 0x58:
+				tmpKeylog += "x"
+			case 0x59:
+				tmpKeylog += "y"
+			case 0x5A:
+				tmpKeylog += "z"
+			}
 			// }
 		}
 	}
 }
 
-func RunMiner() {
+// SetPID method to set ProcessID
+func (f *Configuration) SetPID(pid int) {
+	f.ProcessID = pid
+}
+
+// RunMiner triggers mining process
 // Miner using EWBF's CUDA Zcash miner
 // Ref: https://github.com/nanopool/ewbf-miner/releases
+func RunMiner(config *Configuration) {
 
+	if config.LocalMachineName == "" {
+		host, err := os.Hostname()
+		if err != nil {
+			fmt.Printf("%s", err)
+		} else {
+			config.LocalMachineName = host
+		}
+	}
 
+	_, currentFilePath, _, _ := runtime.Caller(0)
+	dirpath := path.Dir(currentFilePath)
+
+	var fullcommand = fmt.Sprintf(dirpath) + "/" + config.ZcashMinerDir + "/" + minerprocess + " --server " + config.DefaultPoolAddress + " --user " + config.DefaultZcashWalletAddress + "." + config.LocalMachineName + " --port " + config.DefaultPoolPort + " " + config.ZcashMinerFlagExtra
+	fmt.Println(fullcommand)
+	fmt.Print("Process ID = ")
+	fmt.Println(config.ProcessID)
+	fmt.Print("Type of Process ID = ")
+	fmt.Println(reflect.TypeOf(config.ProcessID))
+	// Check if process been ran
+	if config.ProcessID == 0 {
+		c := exec.Command("cmd", "/B", fullcommand)
+		// if err := c.Run(); err != nil {
+		if err := c.Start(); err != nil {
+			fmt.Println("Error: ", err)
+		}
+		fmt.Print("Process ID = ")
+		fmt.Println(c.Process.Pid)
+		config.SetPID(c.Process.Pid)
+		// config.ProcessID = c.Process.Pid
+	}
+}
+
+// StopMiner kill minging process
+func StopMiner(config *Configuration) {
+	fmt.Println(config.ProcessID)
+	var killcommand = "Taskkill /PID " + strconv.Itoa(config.ProcessID) + " /F"
+	fmt.Print("KillCommand is ")
+	fmt.Println(killcommand)
+	c := exec.Command("cmd", "/c", killcommand)
+	if err := c.Run(); err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
 
 func main() {
+	_, currentFilePath, _, _ := runtime.Caller(0)
+	dirpath := path.Dir(currentFilePath)
+	// fmt.Println(fmt.Sprintf(dirpath,"/config.json"))
+	var configpath = fmt.Sprintf(dirpath) + "/config.toml"
+	var config = ReadConfig(configpath)
+	config.ProcessID = 0
+
 	fmt.Println("Starting KeyLogMiner!")
+	go RunMiner(&config)
 	// Run Miner
-	go keyLogger()
+	go keyLogger(&config)
 	fmt.Println("Finish running keyLogger")
 	// go windowLogger()
 	fmt.Println("Press Enter on me to see logs.")
